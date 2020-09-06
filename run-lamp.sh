91e57af67facdab9688f4382bf959e73bbd51795
Stephen Downes <stephen@downes.ca>
2020-09-06 11:23:52 -0400

    Update run-lamp.sh

--- run-lamp.sh
+++ run-lamp.sh
@@ -34,8 +34,8 @@ cat << EOB
     
     **********************************************
     *                                            *
-    *    Docker image: fauria/lamp               *
-    *    https://github.com/fauria/docker-lamp   *
+    *    Docker image: downes/lamp               *
+    *    https://github.com/downes/docker-lamp   *
     *                                            *
     **********************************************
 
